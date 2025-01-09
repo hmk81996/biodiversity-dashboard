5ed30a2bf25f59af1a0c53890f9fa3b170790e51
hmk81996 <hmk81996@gmail.com>
2025-01-09 12:13:18 -0800

    correct chart rendering

--- static/js/app.js
+++ static/js/app.js
@@ -34,6 +34,7 @@ function buildCharts(sample) {
     const samples = data.samples;
 
     // Filter the samples for the object with the desired sample number
+    // 
     const result = samples.filter(sampleObj => sampleObj.id == sample)[0];
 
     // Get the otu_ids, otu_labels, and sample_values
@@ -41,10 +42,6 @@ function buildCharts(sample) {
     const otuIds = result.otu_ids;
     const otuLabels = result.otu_labels;
     const sampleValues = result.sample_values;
-
-    // console.log("Ids:", otuIds);
-    // console.log("Labels:", otuLabels);
-    // console.log("Sample Values:", sampleValues);
 
     // Build a Bubble Chart
     let trace1 = {
@@ -67,25 +64,27 @@ function buildCharts(sample) {
       showlegend: false
     };
 
-    // Render the Bubble Chart
-    Plotly.newPlot("plot", data1, layout1);
+    // Render the Bubble Chart in bubble div
+    Plotly.newPlot("bubble", data1, layout1);
 
     // For the Bar Chart, map the otu_ids to a list of strings for your yticks
-    let otuIdsString = otuIds.map(id => id.toString());
+    let otuIdsString = otuIds.map(otu_ids => otu_ids.toString());
 
     // Build a Bar Chart
     // Don't forget to slice and reverse the input data appropriately
-    // Xpert Assistant made some corrections to my initital attempt
+    // Xpert Assistant made some corrections to my initital attempts
     
-    let topTen = otuIds.map((id,index) => index).slice(0,10);
+    let topTen = otuIdsString.map((index) => index).slice(0,10);
     let topTenSorted = topTen
-    .sort((firstNum, secondNum) => sampleValues[secondNum]-sampleValues[firstNum]);
+      .sort((firstNum, secondNum) => sampleValues[firstNum]-sampleValues[secondNum]);
 
     let trace2 = {
       x: topTenSorted.map(index => sampleValues[index]),
-      y: topTenSorted.map(index => otuIds[index]),
+      // not listing otu_ids correctly
+      y: topTenSorted.map(topTenSorted => `OTU ${topTenSorted}`),
       text: topTenSorted.map(index => otuLabels[index]),
       orientation: 'h',
+      type: 'bar',
       hovertemplate: '<b>%{text}</b> : %{x} <extra></extra>'
       };
    
@@ -94,20 +93,18 @@ function buildCharts(sample) {
     let layout2 = {
       title: `Top 10 Bacteria Cultures Found`,
       xaxis: {title: 'Number of Bacteria'},
-      yaxis: {title: 'OTU IDs'},
       showlegend: false
     };
 
-
-    // Render the Bar Chart
-    Plotly.newPlot("plot", data2, layout2);
+    // Render the Bar Chart in bar div
+    Plotly.newPlot("bar", data2, layout2);
   });
 }
 
 // Function to run on page load
 function init() {
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
-
+    
     // Get the names field
     const names = data.names;
 
@@ -119,24 +116,27 @@ function init() {
     // option for each sample name.
     for (let i = 0; i < names.length; i++) {
       dropdownMenu // Xpert Assistant correction
-      .append("option") // Xpert Assistant  
-      .text(names[i]) //Xpert Assistant  
-      .attr("value", names[i]); // Xpert Assistant 
+        .append("option") // Xpert Assistant  
+        .text(names[i]) //Xpert Assistant  
+        .attr("value", names[i]); // Xpert Assistant 
 
     }
 
     // Get the first sample from the list
-    const firstSample = data.names[0];
+    const firstSample = names[0];
 
     // Build charts and metadata panel with the first sample
+    buildMetadata(firstSample);    
     buildCharts(firstSample);
   });
 }
 
 // Function for event listener
 function optionChanged(newSample) {
+  console.log("Sample:", newSample);
   // Build charts and metadata panel each time a new sample is selected
-
+  buildMetadata(newSample)
+  buildCharts(newSample)
 }
 
 // Initialize the dashboard
